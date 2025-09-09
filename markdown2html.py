7f719736750f14e128347f81f7bb3667386228f5
Nihad ALiyev <lnihad257@gmail.com>
2025-09-10 01:31:26 +0400

    MarkDown

--- markdown2html.py
+++ markdown2html.py
@@ -1,144 +1,131 @@
 #!/usr/bin/python3
 """
-markdown2html module
+Markdown to HTML converter
 """
 import sys
 import os
+import re
 import hashlib
 
 
-def main():
-    """
-    Converts a Markdown heading to an HTML heading
-    """
+def parse_inline(text):
+    """Handle inline formatting: bold, em, [[md5]], ((remove c))"""
+    # Bold **text**
+    text = re.sub(r"\*\*(.+?)\*\*", r"<b>\1</b>", text)
+    # Emphasis __text__
+    text = re.sub(r"__(.+?)__", r"<em>\1</em>", text)
+    # [[text]] -> md5
+    text = re.sub(r"\[\[(.+?)\]\]",
+                  lambda m: hashlib.md5(m.group(1).encode()).hexdigest(),
+                  text)
+    # ((text)) -> remove c/C
+    text = re.sub(r"\(\((.+?)\)\)",
+                  lambda m: m.group(1).replace("c", "").replace("C", ""),
+                  text)
+    return text
 
+
+def convert_markdown(lines):
+    """Convert Markdown lines to HTML lines"""
+    html = []
+    in_ul = False
+    in_ol = False
+    in_p = False
+
+    for line in lines:
+        line = line.rstrip("\n")
+
+        # Başlıqlar
+        if line.startswith("#"):
+            level = len(line.split(" ")[0])
+            if 1 <= level <= 6:
+                if in_p:
+                    html.append("</p>")
+                    in_p = False
+                html.append(
+                    f"<h{level}>{parse_inline(line[level:].strip())}</h{level}>")
+                continue
+
+        # Unordered list (- )
+        if line.startswith("- "):
+            if in_p:
+                html.append("</p>")
+                in_p = False
+            if not in_ul:
+                html.append("<ul>")
+                in_ul = True
+            html.append(f"<li>{parse_inline(line[2:].strip())}</li>")
+            continue
+        else:
+            if in_ul:
+                html.append("</ul>")
+                in_ul = False
+
+        # Ordered list (* )
+        if line.startswith("* "):
+            if in_p:
+                html.append("</p>")
+                in_p = False
+            if not in_ol:
+                html.append("<ol>")
+                in_ol = True
+            html.append(f"<li>{parse_inline(line[2:].strip())}</li>")
+            continue
+        else:
+            if in_ol:
+                html.append("</ol>")
+                in_ol = False
+
+        # Boş sətir -> paraqraf bağla
+        if line.strip() == "":
+            if in_p:
+                html.append("</p>")
+                in_p = False
+            continue
+
+        # Paraqraf
+        if not in_p:
+            html.append("<p>")
+            html.append(parse_inline(line))
+            in_p = True
+        else:
+            html.append("<br/>")
+            html.append(parse_inline(line))
+
+    # Açıq tag-ları bağla
+    if in_ul:
+        html.append("</ul>")
+    if in_ol:
+        html.append("</ol>")
+    if in_p:
+        html.append("</p>")
+
+    return html
+
+
+def main():
+    """Main entry point"""
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         sys.exit(1)
 
-    if not os.path.isfile(sys.argv[1]):
-        print(f'Missing {sys.argv[1]}', file=sys.stderr)
+    infile, outfile = sys.argv[1], sys.argv[2]
+
+    if not os.path.isfile(infile):
+        print(f"Missing {infile}", file=sys.stderr)
         sys.exit(1)
 
-    with open(sys.argv[1], 'r') as f:
-        content = f.readlines()
+    with open(infile, "r", encoding="utf-8") as f:
+        lines = f.readlines()
 
-    line_html_1 = []
-    m = 0
-    for line in content:
-        if line[:2] == '* ':
-            if m == 0:
-                m = 1
-                line_html_1.append('<ol>')
-            line_html_1.append(f"<li>{line[2:].strip()}</li>")
-        else:
-            if m == 1:
-                m = 0
-                line_html_1.append("</ol>")
-            line_html_1.append(line)
-    if m == 1:
-        line_html_1.append("</ol>")
+    html_lines = convert_markdown(lines)
 
-    line_html = []
-    m = 0
-    for line in line_html_1:
-        if line[0] == '-':
-            if m == 0:
-                m = 1
-                line_html.append('<ul>')
-            line_html.append(f"<li>{line[2:].strip()}</li>")
-        else:
-            if m == 1:
-                m = 0
-                line_html.append("</ul>")
-            line_html.append(line)
-    if m == 1:
-        line_html.append("</ul>")
-
-    html_lines = []
-    for i in line_html:
-        if i[0] == '#':
-            b = 0
-            for a in i:
-                if a == '#':
-                    b += 1
-            if b > 0 and b <= 6:
-                text = f"<h{b}>{i[b:].strip()}</h{b}>"
-                # print(text)
-                html_lines.append(text)
-        else:
-            html_lines.append(i)
-
-    line_html_2 = []
-    m = 0
-    for line in html_lines:
-        if line[:2] == "**" or line[:2] == "((" or line[0].isalpha():
-            if m == 0:
-                m = 1
-                line_html_2.append('<p>')
-                line_html_2.append(line.strip())
-            else:
-                line_html_2.append(f'<br/>\n{line.strip()}')
-        else:
-            if m == 1:
-                m = 0
-                line_html_2.append("</p>")
-            if line[0] != '\n':
-                line_html_2.append(line)
-    if m == 1:
-        line_html_2.append("</p>")
-
-    line_html_4 = []
-    for i in line_html_2:
-        i = i.replace('**', '<b>', 1)
-        i = i.replace('**', '</b>', 1)
-        i = i.replace('__', '<em>', 1)
-        i = i.replace('__', '</em>', 1)
-        line_html_4.append(i)
-
-    line_html_5 = []
-
-    for i in line_html_4:
-        k = 0
-        if '[[' in i and ']]' in i:
-            start_index = i.find("[[") + 2
-            end_index = i.find("]]")
-            hash_content = i[start_index:end_index]
-            hashed_content = hashlib.md5(hash_content.encode()).hexdigest()
-
-            text = i[:i.find("[")] + hashed_content + i[i.find("]") + 2:]
-
-            # text = i[:i.find("[")] + hashed_content + i[i.find("]") + 2:]
-            # text = i[:i.find("[")] + hashlib.md5(i[i.find("[[")
-            # + 2: i.find("]]")
-            # ].encode()).hexdigest() + i[i.find("]") + 2:]
-
-            line_html_5.append(text.strip())
-            k = 1
-        if '((' in i and '))' in i:
-            cleaned = i.replace(
-                'c',
-                '').replace(
-                'C',
-                '').replace(
-                ')',
-                '').replace(
-                '(',
-                '')
-            line_html_5.append(cleaned.strip())
-            k = 1
-        if k == 0:
-            line_html_5.append(i)
-
-    with open(sys.argv[2], 'w') as f:
-        f.write("\n".join(line_html_5))
+    with open(outfile, "w", encoding="utf-8") as f:
+        f.write("\n".join(html_lines))
 
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    """
-    markdown2html module
-    """
     main()
